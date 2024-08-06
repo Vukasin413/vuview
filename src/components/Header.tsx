3a6b8d328419db0ff0a02dc84948845590e4f3ad
Vukasin413 <122133860+Vukasin413@users.noreply.github.com>
2024-08-06 23:21:18 +0200

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -438,12 +438,12 @@ export default function Header() {
       <div class="flex items-center justify-between w-full h-full py-2 max-w-full min-w-0">
         <Link
           href={`/${preferences.content.defaultHomePage?.toLowerCase() ?? "trending"}`}
-          class="text-text1 mx-2 w-18 h-8"
+          class="text-text1 mx-2 w-22 h-8"
         >
           <img
             src="/logo.svg"
             alt="Conduit"
-            class="h-8 w- "
+            class="h-10 w-8 "
           />
         </Link>
         <Search />
